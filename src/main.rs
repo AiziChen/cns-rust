f7c49925e8bb9da9187123fc8f252b510e267c78
aizichen <1098679804@qq.com>
2022-09-02 07:38:23 +0800

    better format for error

--- src/main.rs
+++ src/main.rs
@@ -23,17 +23,17 @@ mod udp;
 async fn response_header(stream: &mut TcpStream, buf: &[u8]) -> bool {
     if bytes_contains(&buf, "WebSocket".as_bytes()) {
         if let Err(e) = stream.write_all("HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: CuteBi Network Tunnel, (%>w<%)\r\n\r\n".as_bytes()).await {
-            error!("failed to write to socket; err = {:?}", e);
+            error!("failed to write to socket; err = {}", e.to_string());
             return false;
         }
     } else if bytes_contains(&buf, "CON".as_bytes()) {
         if let Err(e) = stream.write_all("HTTP/1.1 200 Connection established\r\nServer: CuteBi Network Tunnel, (%>w<%)\r\nConnection: keep-alive\r\n\r\n".as_bytes()).await {
-            error!("failed to write to socket; err = {:?}", e);
+            error!("failed to write to socket; err = {}", e.to_string());
             return false;
         }
     } else {
         if let Err(e) = stream.write_all("HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\nServer: CuteBi Network Tunnel, (%>w<%)\r\nConnection: keep-alive\r\n\r\n".as_bytes()).await {
-            error!("failed to write to socket; err = {:?}", e);
+            error!("failed to write to socket; err = {}", e.to_string());
             return false;
         }
     }
@@ -47,8 +47,8 @@ async fn handle_connection(mut stream: &mut TcpStream) {
     let len = match (&mut stream).read(&mut buf).await {
         Ok(len) if len == 0 => return,
         Ok(len) => len,
-        Err(e) => {
-            error!("failed to read from socket; err = {:?}", e);
+        Err(err) => {
+            error!("failed to read from socket; reason = {}", err.to_string());
             return;
         }
     };
